67e64f19d4ffc35ca14df60804e7e094cc7a3649
tegaryudha11 <67151421+tegaryudha11@users.noreply.github.com>
2020-09-10 11:59:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
         })
-    }, 18 * 1000);
+    }, 15 * 1000);
